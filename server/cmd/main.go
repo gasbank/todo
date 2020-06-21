d92b86c68a6de62470474a635c3789df01bba537
saemi-park <saemi.park@devsisters.com>
2020-06-21 11:05:21 +0900

    로컬 DB연결 및 todo추가, 삭제 로직 추직

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,31 +1,71 @@
 package main
 
 import (
-"context"
-"log"
-"net"
+	"context"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"log"
+	"net"
 
-"google.golang.org/grpc"
-pb "github.com/gasbank/todo/protobuf"
+	"google.golang.org/grpc"
+	pb "github.com/gasbank/todo/protobuf"
 )
 
 const (
 port = ":50051"
 )
 
+var db *sqlx.DB
+
 type server struct {
 	pb.UnimplementedTodolistServer
 }
 
-func (s *server) addTodo(ctx context.Context, in *pb.TodoRequest) (*pb.TodoReply, error) {
-	log.Printf("Received: %v", in.GetItem())
-	return &pb.TodoReply{ReponseCode: "Hello " + in.GetItem()}, nil
+func (s *server) AddTodo(ctx context.Context, in *pb.TodoRequest) (*pb.TodoReply, error) {
+	log.Printf("Received: %v", in.GetItem().GetTodo())
+	query := `INSERT todos VALUE ?`
+	_, err := db.Exec(query, in.GetItem().GetTodo())
+	if err != nil {
+		return &pb.TodoReply{ResponseCode: "50000" + in.GetItem().GetTodo()}, err
+	}
+	return &pb.TodoReply{ResponseCode: "20000" + in.GetItem().GetTodo()}, nil
 }
-func (s *server) removeTodo(ctx context.Context, in *pb.TodoRequest) (*pb.TodoReply, error) {
-	return &pb.TodoReply{ReponseCode: "Hello again " + in.GetItem()}, nil
+func (s *server) RemoveTodo(ctx context.Context, in *pb.TodoRequest) (*pb.TodoReply, error) {
+	log.Printf("Received: %v", in.GetItem().GetTodo())
+	query := `DELETE FROM todos WHERE todo=?`
+	_, err := db.Exec(query, in.GetItem().GetTodo())
+	if err != nil {
+		return &pb.TodoReply{ResponseCode: "50000" + in.GetItem().GetTodo()}, err
+	}
+	return &pb.TodoReply{ResponseCode: "Hello again " + in.GetItem().GetTodo()}, nil
+}
+func (s *server) GetTodoAll(ctx context.Context, in *pb.EmptyRequest) (*pb.TodoAllResponse, error) {
+	log.Println("Received: %v")
+	return &pb.TodoAllResponse{}, nil
 }
 
+
 func main() {
+
+	var err error
+	mysqlConfig := &mysql.Config{
+		User:                 "root",
+		Passwd:               "password",
+		Net:                  "tcp",
+		Addr:                 fmt.Sprintf("%s:%s", "127.0.0.1", "3306"),
+		DBName:               "todo_db",
+		AllowNativePasswords: true,
+	}
+
+	dsn := mysqlConfig.FormatDSN()
+
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("err in %v: %v", db, err)
+	}
+
+	fmt.Println("started")
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 	log.Fatalf("failed to listen: %v", err)
---
+++ server/go.mod
@@ -0,0 +1,11 @@
+module github.com/gasbank/todo/server
+
+go 1.13
+
+require (
+	github.com/gasbank/todo v0.0.0-20200620085451-187cfec483bf // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+	google.golang.org/grpc v1.29.1 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
